60c6e910e87dab45236b07efaeec72fe2bd0c0e8
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-10 01:00:42 -0300

    Created categoria model

---
+++ src/main/java/com/vicente/algamoney/api/model/Categoria.java
@@ -0,0 +1,25 @@
+package com.vicente.algamoney.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode
+@Getter @Setter
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String name;
+	
+}
