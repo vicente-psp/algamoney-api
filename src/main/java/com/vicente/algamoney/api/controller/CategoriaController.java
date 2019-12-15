c1b287d597362becc6f01d1a214aadf296248ca7
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-14 22:49:07 -0300

    Update CategoriaController.java
    Criado os métodos findById, create e deleteById.

--- src/main/java/com/vicente/algamoney/api/controller/CategoriaController.java
+++ src/main/java/com/vicente/algamoney/api/controller/CategoriaController.java
@@ -1,11 +1,23 @@
 package com.vicente.algamoney.api.controller;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vicente.algamoney.api.model.Categoria;
 import com.vicente.algamoney.api.repository.CategoriaRepository;
@@ -19,6 +31,27 @@ public class CategoriaController {
 	@GetMapping
 	public List<Categoria> listAll() {
 		return categoriaRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Categoria> findById(@PathVariable Long id) {
+		return categoriaRepository.findById(id);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria entity) {
+		Categoria savedEntity = categoriaRepository.save(entity);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+		.buildAndExpand(savedEntity.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(savedEntity);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void deleteById(@PathVariable Long id) {
+		categoriaRepository.deleteById(id);
 	}
 	
 }
