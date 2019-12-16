d0859e1f832eca0f4a7d2a4d0af5f88ea2cc424d
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-15 22:06:56 -0300

    Create Endereco.java
    Criado model endereco que compõem recurso Pessoa.

---
+++ src/main/java/com/vicente/algamoney/api/model/Endereco.java
@@ -0,0 +1,38 @@
+package com.vicente.algamoney.api.model;
+
+import javax.persistence.Embeddable;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Embeddable
+public class Endereco {
+	
+	@Size(min = 3, max = 30)
+	private String logradouro;
+	
+	@Size(min = 1, max = 30)
+	private String numero;
+	
+	@Size(min = 3, max = 30)
+	private String complemento;
+	
+	@Size(min = 3, max = 30)
+	private String bairro;
+	
+	@Size(min = 8, max = 10)
+	private String cep;
+	
+	@Size(min = 3, max = 30)
+	private String cidade;
+	
+	@Size(min = 3, max = 30)
+	private String estado;
+
+}
