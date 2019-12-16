a6a634ab3e0ce6d22e0cf67218435ac9c3f154d2
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-15 22:06:33 -0300

    Create Pessoa.java
    Criado model para recurso Pessoa.

---
+++ src/main/java/com/vicente/algamoney/api/model/Pessoa.java
@@ -0,0 +1,42 @@
+package com.vicente.algamoney.api.model;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@EqualsAndHashCode(exclude = {"nome", "endereco"})
+@ToString
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "tb_pessoa")
+public class Pessoa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 3, max = 50)
+	private String nome;
+	
+	@NotNull
+	private Boolean ativo;
+	
+	@Embedded
+	private Endereco endereco;
+}
