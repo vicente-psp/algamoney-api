4b9607ea9bedffe0c4115addb7308b06e40fa45a
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-16 23:10:49 -0300

    Implementado método handleEmptyResultDataAccessException.

--- src/main/java/com/vicente/algamoney/api/exceptionhandler/AlgaMoneyExceptionHandler.java
+++ src/main/java/com/vicente/algamoney/api/exceptionhandler/AlgaMoneyExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -40,6 +42,14 @@ public class AlgaMoneyExceptionHandler extends ResponseEntityExceptionHandler {
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		List<Erro> errList = createErrorList(ex.getBindingResult());
 		return handleExceptionInternal(ex, errList, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@ExceptionHandler({EmptyResultDataAccessException.class})
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
+		String userMessage = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+		String developerMessage = ex.toString();
+		List<Erro> errList = Arrays.asList(new Erro(userMessage, developerMessage));
+		return handleExceptionInternal(ex, errList, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 	
 	
