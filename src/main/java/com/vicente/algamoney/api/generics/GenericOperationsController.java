0858a5c1091ad528040e811a2b5d8e402ac98d51
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-16 22:36:16 -0300

    Update GenericOperationsController.java
    Adicionado parâmetro HttpServletResponse no método post.

--- src/main/java/com/vicente/algamoney/api/generics/GenericOperationsController.java
+++ src/main/java/com/vicente/algamoney/api/generics/GenericOperationsController.java
@@ -2,12 +2,14 @@ package com.vicente.algamoney.api.generics;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.ResponseEntity;
 
 
 public interface GenericOperationsController<E> {
 	
-	ResponseEntity<E> post(E entity);
+	ResponseEntity<E> post(E entity, HttpServletResponse response);
 	ResponseEntity<E> get(Long id);
 	ResponseEntity<E> put(Long id, E entity);
 	ResponseEntity<?> delete(Long id);
