62a78ec1e33da81117ec66ec7499c6df1da6e367
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-18 22:28:45 -0300

    Alterado método post e implementado métodos que capturam exceções na classe de serviço de lançamento.

--- src/main/java/com/vicente/algamoney/api/controller/LancamentoController.java
+++ src/main/java/com/vicente/algamoney/api/controller/LancamentoController.java
@@ -1,5 +1,6 @@
 package com.vicente.algamoney.api.controller;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,9 +9,13 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,9 +24,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vicente.algamoney.api.event.RecursoCriadoEvent;
+import com.vicente.algamoney.api.exceptionhandler.AlgaMoneyExceptionHandler.Erro;
 import com.vicente.algamoney.api.generics.GenericOperationsController;
 import com.vicente.algamoney.api.model.Lancamento;
 import com.vicente.algamoney.api.repository.LancamentoRepository;
+import com.vicente.algamoney.api.service.LancamentoService;
+import com.vicente.algamoney.api.service.exception.PessoaInativaException;
+import com.vicente.algamoney.api.service.exception.PessoaInexistenteException;
 
 @RestController
 @RequestMapping("/lancamentos")
@@ -29,7 +38,11 @@ public class LancamentoController implements GenericOperationsController<Lancame
 
 	@Autowired private LancamentoRepository repository;
 	
+	@Autowired private LancamentoService service;
+	
 	@Autowired private ApplicationEventPublisher publisher;
+	
+	@Autowired private MessageSource messageSource;
 
 	@GetMapping
 	public List<Lancamento> get() {
@@ -48,7 +61,7 @@ public class LancamentoController implements GenericOperationsController<Lancame
 	
 	@PostMapping
 	public ResponseEntity<Lancamento> post(@Valid @RequestBody Lancamento entity, HttpServletResponse response) {
-		Lancamento savedEntity = repository.save(entity);
+		Lancamento savedEntity = service.save(entity);
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, savedEntity.getId()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(savedEntity);
 	}
@@ -63,6 +76,22 @@ public class LancamentoController implements GenericOperationsController<Lancame
 	public void delete(Long id) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	@ExceptionHandler({PessoaInexistenteException.class})
+	public ResponseEntity<Object> handlePessoaInexistenteException(PessoaInexistenteException ex) {
+		String userMessage = messageSource.getMessage("pessoa.inexistente", null, LocaleContextHolder.getLocale());
+		String developerMessage = ex.toString();
+		List<Erro> errList = Arrays.asList(new Erro(userMessage, developerMessage));
+		return ResponseEntity.badRequest().body(errList);
+	}
+	
+	@ExceptionHandler({PessoaInativaException.class})
+	public ResponseEntity<Object> handlePessoaInativaException(PessoaInativaException ex) {
+		String userMessage = messageSource.getMessage("pessoa.inativa", null, LocaleContextHolder.getLocale());
+		String developerMessage = ex.toString();
+		List<Erro> errList = Arrays.asList(new Erro(userMessage, developerMessage));
+		return ResponseEntity.badRequest().body(errList);
 	}
 
 }
