a4f64c700733f8999b71e0d0f7407318cf113efd
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-18 21:37:41 -0300

    Criado método para tratar exceções do tipo DataIntegrityViolationException.

--- src/main/java/com/vicente/algamoney/api/exceptionhandler/AlgaMoneyExceptionHandler.java
+++ src/main/java/com/vicente/algamoney/api/exceptionhandler/AlgaMoneyExceptionHandler.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -50,6 +52,14 @@ public class AlgaMoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		String developerMessage = ex.toString();
 		List<Erro> errList = Arrays.asList(new Erro(userMessage, developerMessage));
 		return handleExceptionInternal(ex, errList, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+	}
+	
+	@ExceptionHandler({DataIntegrityViolationException.class})
+	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
+		String userMessage = messageSource.getMessage("recurso.operacao-nao-permitida", null, LocaleContextHolder.getLocale());
+		String developerMessage = ExceptionUtils.getRootCauseMessage(ex);
+		List<Erro> errList = Arrays.asList(new Erro(userMessage, developerMessage));
+		return handleExceptionInternal(ex, errList, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 	
 	
