e3442ff23655cebff15fea4243cbd5bbe7ca8d6a
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-20 21:52:09 -0300

    Implementação da segurança.

---
+++ src/main/java/com/vicente/algamoney/api/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.vicente.algamoney.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("admin").password("admin").roles("ROLE");
+		
+		super.configure(auth);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/categorias").permitAll()
+			.anyRequest().authenticated()
+			.and()
+		.httpBasic().and()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		.csrf().disable();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+	    User.UserBuilder builder = User.withDefaultPasswordEncoder();
+	    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+	    manager.createUser(builder.username("admin").password("admin").roles("ROLE").build());
+	    return manager;
+	}
+	
+}
