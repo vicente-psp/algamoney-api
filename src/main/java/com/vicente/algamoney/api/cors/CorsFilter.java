1d0cbe2d72808e8a728eac79100361ec1639a283
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-23 23:08:30 -0300

    Criação das classes.

---
+++ src/main/java/com/vicente/algamoney/api/cors/CorsFilter.java
@@ -0,0 +1,43 @@
+package com.vicente.algamoney.api.cors;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+	private String originAllow = "http://localhost:8080";
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		
+		resp.setHeader("Access-Control-Allow-Origin", originAllow);
+		
+		if ("OPTIONS".equals(req.getMethod()) && originAllow.equals(req.getHeader("Origin"))) {
+			resp.setHeader("Access-Control-Allow-Methods", "POST, GET, ,DELETE, PUT, OPTIONS");
+			resp.setHeader("Access-Control-Allow-Headers", "Authorizarion, Content-Type, Accept");
+			resp.setHeader("Access-Control-Max-Age", "3600");
+			
+			resp.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+
+}
