f4ed58f1d695d0bf70c1307be8358074993f6813
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-31 14:38:31 -0300

    Adicionado configuração padrão para TimeZone.

--- src/main/java/com/vicente/algamoney/api/AlgamoneyApiApplication.java
+++ src/main/java/com/vicente/algamoney/api/AlgamoneyApiApplication.java
@@ -1,5 +1,9 @@
 package com.vicente.algamoney.api;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -12,6 +16,11 @@ public class AlgamoneyApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlgamoneyApiApplication.class, args);
+	}
+	
+	@PostConstruct
+  public void init(){
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC-3"));
 	}
 
 }
