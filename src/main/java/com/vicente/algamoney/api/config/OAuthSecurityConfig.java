085d10ec1c40a4e348014a9e0b334454ca0432c6
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-31 23:25:42 -0300

    Adicionado anotação Profile com oauth-security.

--- src/main/java/com/vicente/algamoney/api/config/OAuthSecurityConfig.java
+++ src/main/java/com/vicente/algamoney/api/config/OAuthSecurityConfig.java
@@ -2,6 +2,7 @@ package com.vicente.algamoney.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@Profile("oauth-security")
 public class OAuthSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
