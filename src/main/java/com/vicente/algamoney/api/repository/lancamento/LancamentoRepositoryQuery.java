d4857d977d0970d385564ddbf0fae9515954b09a
Vicente Silva <vicente.pspenha@gmail.com>
2020-02-02 22:34:23 -0200

    Criado método porCategoria para listar lancamentos agrupados por categorias.

--- src/main/java/com/vicente/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/vicente/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,14 +1,19 @@
 package com.vicente.algamoney.api.repository.lancamento;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.vicente.algamoney.api.dto.LancamentoEstatisticaCategoria;
 import com.vicente.algamoney.api.model.Lancamento;
 import com.vicente.algamoney.api.repository.filter.LancamentoFilter;
 import com.vicente.algamoney.api.repository.projection.ResumoLancamento;
 
 public interface LancamentoRepositoryQuery {
 
+	public List<LancamentoEstatisticaCategoria> porCategoria(LocalDate mesReferencia);
 	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 	public Page<ResumoLancamento> resumo(LancamentoFilter lancamentoFilter, Pageable pageable);
 	
