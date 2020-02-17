c05c11201dfbc170b2c53bdfa784f9d38bc437f0
Vicente Silva <vicente.pspenha@gmail.com>
2020-02-16 22:43:28 -0300

    Criação de DTO LancamentoEstatisticaPessoa.

---
+++ src/main/java/com/vicente/algamoney/api/dto/LancamentoEstatisticaPessoa.java
@@ -0,0 +1,24 @@
+package com.vicente.algamoney.api.dto;
+
+import java.math.BigDecimal;
+
+import com.vicente.algamoney.api.model.Pessoa;
+import com.vicente.algamoney.api.model.TipoLancamento;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LancamentoEstatisticaPessoa {
+	
+	private TipoLancamento tipoLancamento;
+	
+	private Pessoa pessoa;
+	
+	private BigDecimal total;
+
+}
